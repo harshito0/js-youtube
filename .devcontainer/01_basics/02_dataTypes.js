99d403ae3f174ab91d143829bdde122a3f2ae124
harshito0 <harshitsingh190622@gmail.com>
2024-12-25 05:15:49 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+ "use strict";   //terat all js  code as  newer version
+//   alert(3+3)  we are using node js not browser
+console.log(3+3);
+let name="harshit"
+let age =20
+let isLoggedIn =true
+console.log([age,isLoggedIn]);
